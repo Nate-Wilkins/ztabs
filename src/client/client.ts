704c826764c273393eb482c8b0753ca8c505ce6b
nate-wilkins <nate-wilkins@code-null.com>
2023-01-12 03:18:11 -0500

    Update 'client' with todo for trustedtypes api

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,3 +1,9 @@
 import pace from 'pace-js';
 
+// TODO: Support for trystedTypes API - even though its stupid.
+// escapeHTMLPolicy = trustedTypes.createPolicy("forceInner", {
+//   createHTML: (to_escape) => to_escape
+// })
+// my_element.innerHTML = escapeHTMLPolicy.createHTML("<h1>your_html</h1>");
+// this.el.innerHTML = '<div class="pace-progress'+_custom_class_name+'">\n  <div class="pace-progress-inner"></div>\n</div>\n<div class="pace-activity"></div>';
 pace.start();
